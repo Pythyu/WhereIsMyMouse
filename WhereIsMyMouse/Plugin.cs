9d169ac070f13d29e4074867d0f8d0c1cb132118
Marethyu <antoine.zellmeyer@epita.fr>
2022-09-03 19:09:48 +0200

    files

---
+++ WhereIsMyMouse/Plugin.cs
@@ -0,0 +1,58 @@
+﻿using Dalamud.Game.Command;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using System.IO;
+using System.Reflection;
+
+namespace WhereIsMyMouse
+{
+    public sealed class Plugin : IDalamudPlugin
+    {
+        public string Name => "Where's my mouse";
+
+        private const string commandName = "/wmm";
+
+        private DalamudPluginInterface PluginInterface { get; init; }
+        private CommandManager CommandManager { get; init; }
+        private Configuration Configuration { get; init; }
+        private PluginUI PluginUi { get; init; }
+
+        public Plugin(
+            [RequiredVersion("1.0")] DalamudPluginInterface pluginInterface,
+            [RequiredVersion("1.0")] CommandManager commandManager)
+        {
+            this.PluginInterface = pluginInterface;
+            this.CommandManager = commandManager;
+
+            this.Configuration = this.PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
+            this.Configuration.Initialize(this.PluginInterface);
+            
+            this.PluginUi = new PluginUI(this.Configuration);
+
+            this.CommandManager.AddHandler(commandName, new CommandInfo(OnCommand)
+            {
+                HelpMessage = "Open Where's my mouse setting menu"
+            });
+            
+            this.PluginInterface.UiBuilder.Draw += DrawUI;
+        }
+
+        public void Dispose()
+        {
+            this.PluginUi.Dispose();
+            this.CommandManager.RemoveHandler(commandName);
+        }
+
+        private void OnCommand(string command, string args)
+        {
+            // in response to the slash command, just display our main ui
+            this.PluginUi.Visible = true;
+        }
+
+        private void DrawUI()
+        {
+            this.PluginUi.Draw();
+        }
+        
+    }
+}
---
+++ WhereIsMyMouse/PluginUI.cs
@@ -0,0 +1,101 @@
+﻿using ImGuiNET;
+using System;
+using System.Drawing;
+using System.Numerics;
+using Dalamud.Logging;
+
+namespace WhereIsMyMouse
+{
+    // It is good to have this be disposable in general, in case you ever need it
+    // to do any cleanup
+    class PluginUI : IDisposable
+    {
+        private Configuration configuration;
+
+        // this extra bool exists for ImGui, since you can't ref a property
+        private bool visible = false;
+
+        private bool CursorOn = false;
+
+        private float size = 10;
+
+        private float thickness = 5;
+
+        private Vector4 color = new Vector4(1, 0, 0, 1);
+        
+        public bool Visible
+        {
+            get { return this.visible; }
+            set { this.visible = value; }
+        }
+        
+
+        // passing in the image here just for simplicity
+        public PluginUI(Configuration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void Draw()
+        {
+
+
+            DrawMainWindow();
+            CursorAura();
+        }
+        
+        private uint ToUint(Vector4 c)
+        {
+            uint total = ((uint)(c.W*255) << 24) + ((uint)(c.Z*255) << 16) + ((uint)(c.Y*255) << 8) + (uint)(c.X*255);
+            return total;
+        }
+
+        public void CursorAura()
+        {
+            if (!CursorOn)
+            {
+                return;
+            }
+
+            Vector2 cursorPos = ImGui.GetMousePos();
+            ImGui.SetNextWindowSize(new Vector2(300, 300));
+            ImGui.Begin("CursorWindow", ImGuiWindowFlags.NoBackground | ImGuiWindowFlags.NoDecoration | ImGuiWindowFlags.NoInputs);
+            ImGui.SetWindowPos(cursorPos - new Vector2(150,150));
+
+            var draw = ImGui.GetWindowDrawList();
+            draw.AddCircle(cursorPos, size, this.ToUint(this.color), 0, this.thickness);
+            ImGui.End();
+        }
+        
+
+        public void DrawMainWindow()
+        {
+            if (!Visible)
+            {
+                return;
+            }
+
+            ImGui.SetNextWindowSize(new Vector2(375, 330), ImGuiCond.FirstUseEver);
+            ImGui.SetNextWindowSizeConstraints(new Vector2(375, 330), new Vector2(float.MaxValue, float.MaxValue));
+            if (ImGui.Begin("Where's my mouse settings", ref this.visible, ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse))
+            {
+                ImGui.Text("Cursor Aura : ");
+                ImGui.SameLine();
+                ImGui.Checkbox("###CursorAura", ref this.CursorOn);
+                ImGui.Text("Circle Size : ");
+                ImGui.SameLine();
+                ImGui.SliderFloat("###sizeslide", ref this.size, 0f, 130);
+                ImGui.Text("Thickness : ");
+                ImGui.SameLine();
+                ImGui.SliderFloat("###thickslide", ref this.thickness, 0f, 100f);
+                ImGui.ColorPicker4("###ColorPicker", ref this.color);
+            }
+            ImGui.End();
+        }
+        
+    }
+}
